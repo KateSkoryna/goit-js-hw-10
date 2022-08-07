aeb6fa97364cf98134a70e4648cc4c5da82d01da
Kateryna Skoryna <k.skoryna@gmail.com>
2022-08-07 16:24:16 +0200

    Added second version of box render function

--- src/renderCountryBox.js
+++ src/renderCountryBox.js
@@ -27,3 +27,31 @@ export default function renderCountryBox(list, box) {
     .join('');
   return (box.innerHTML = markup);
 }
+
+// ВТОРОЙ ВАРИАНТ ЧЕРЕЗ ДЕСТРУКТУРИЗАЦИЮ МАССИВА И БЕЗ ПЕРЕБОРА
+
+// export default function renderCountryBox(list, box) {
+//   const [country] = list;
+//   const {
+//     name: { official },
+//     capital,
+//     population,
+//     flags: { svg },
+//     languages,
+//   } = country;
+//   return (box.innerHTML = `<div class="country-info__box"><img src="${svg}" alt="flag" width="30">
+//     <h1 class="country-info__main-title">${official}</h1></div>
+//     <ul class="country-info__list">
+//     <li class="country-info__item">
+//      <h2 class="country-info___title">Capital:</h2>
+//     <p class="country-info___text">${capital}</p>
+//     </li>
+//     <li class="country-info__item">
+//         <h2 class="country-info___title">Population:</h2>
+//      <p class="country-info___text">${population}</p>
+//     </li>
+//     <li class="country-info__item">
+//     <h2 class="country-info___title">Languages:</h2>
+//      <p class="country-info___text">${Object.values(languages)}</p></li>
+//     </ul>`);
+// }
